47f6fe4fe2e5fda0db86a43336dc717c3b24b8d3
objektwerks <objektwerks@runbox.com>
2022-01-25 10:36:53 -0500

    refactored load resource

--- jvm/src/main/scala/objektwerks/poolmate/Router.scala
+++ jvm/src/main/scala/objektwerks/poolmate/Router.scala
@@ -11,21 +11,23 @@ import Serializers.given
 
 import upickle.default.{read, write}
 
-object Router:
+object Router extends LazyLogging:
   private val utf8 = Codec.UTF8.name
-  private val html = loadResource("/public/index.html")
+  private val html = loadResource("index.html")
 
   def loadResource(resource: String): String =
-    Using( Source.fromInputStream(getClass.getResourceAsStream(resource), utf8) ) { 
-    source => source.mkString
-  }.getOrElse(s"*** Failed to load: $resource")
+    val path = s"/publc/$resource"
+    logger.debug(s"*** Load resource: $path")
+    Using( Source.fromInputStream(getClass.getResourceAsStream(path), utf8) ) { 
+      source => source.mkString
+    }.getOrElse(s"*** Failed to load: $path")
 
 class Router(dispatcher: Dispatcher) extends Routes with LazyLogging:
   @cask.get("/")
   def index() = Router.html
 
-  @cask.staticResources("/public")
-  def public() = "."
+  @cask.get("/public")
+  def public(request: cask.Request) = Router.loadResource(request.text())
 
   @cask.post("/command")
   def command(request: Request) =
