bd8a327d0b781a38bef9365f28dbe1f3d34e7287
objektwerks <objektwerks@runbox.com>
2022-01-25 16:32:37 -0500

    removed resource route

--- jvm/src/main/scala/objektwerks/poolmate/Router.scala
+++ jvm/src/main/scala/objektwerks/poolmate/Router.scala
@@ -27,9 +27,6 @@ class Router(dispatcher: Dispatcher) extends Routes with LazyLogging:
   @cask.get("/")
   def index() = cask.Response(Router.html, 200, Seq("Content-Type" -> "text/html; charset=UTF-8"))
 
-  @cask.staticResources("/", headers = Seq("Content-Type" -> "image/png"))
-  def public() = "."
-
   @cask.post("/command")
   def command(request: Request) =
     val command = read[Command](request.text())
