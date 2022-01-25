23b4f1bb05528a1d4513d5890e3204126f33dd5e
objektwerks <objektwerks@runbox.com>
2022-01-25 13:37:39 -0500

    moved web resources to route of classpath

--- jvm/src/main/scala/objektwerks/poolmate/Router.scala
+++ jvm/src/main/scala/objektwerks/poolmate/Router.scala
@@ -27,8 +27,8 @@ class Router(dispatcher: Dispatcher) extends Routes with LazyLogging:
   @cask.get("/")
   def index() = cask.Response(Router.html, 200, Seq("Content-Type" -> "text/html; charset=UTF-8"))
 
-  @cask.staticResources("/public/", headers = Seq("Content-Type" -> "image/png"))
-  def public() = ""
+  @cask.staticResources("/", headers = Seq("Content-Type" -> "image/png"))
+  def public() = "."
 
   @cask.post("/command")
   def command(request: Request) =
