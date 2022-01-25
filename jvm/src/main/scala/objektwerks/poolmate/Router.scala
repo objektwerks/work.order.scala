750ec2d277d1618cd148a0d6a4c3ad9df61b76aa
objektwerks <objektwerks@runbox.com>
2022-01-25 18:02:17 -0500

    readded resources route

--- jvm/src/main/scala/objektwerks/poolmate/Router.scala
+++ jvm/src/main/scala/objektwerks/poolmate/Router.scala
@@ -27,6 +27,9 @@ class Router(dispatcher: Dispatcher) extends Routes with LazyLogging:
   @cask.get("/")
   def index() = cask.Response(Router.html, 200, Seq("Content-Type" -> "text/html; charset=UTF-8"))
 
+  @cask.staticResources(Router.prefix)
+  def resources() = "."
+
   @cask.post("/command")
   def command(request: Request) =
     val command = read[Command](request.text())
