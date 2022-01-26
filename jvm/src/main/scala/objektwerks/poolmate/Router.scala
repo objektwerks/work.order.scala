194d826ff3ca497cfdd6b9db84510834519d7426
objektwerks <objektwerks@runbox.com>
2022-01-26 08:12:16 -0500

    refactored router to create header per file type

--- jvm/src/main/scala/objektwerks/poolmate/Router.scala
+++ jvm/src/main/scala/objektwerks/poolmate/Router.scala
@@ -13,31 +13,39 @@ import upickle.default.{read, write}
 
 object Router extends LazyLogging:
   private val utf8 = Codec.UTF8.name
+  private val contentType = "Content-Type"
   private val prefix = "/public/"
-  private val html = loadResource("index.html")
+  private val indexHtml = loadResource("index.html")
+  private val indexHtmlHeader = contentType -> "text/html; charset=UTF-8"
 
-  def loadResource(resource: String): String =
+  def loadResource(resource: String): Array[Byte] =
     val path = s"$prefix$resource"
-    logger.debug(s"*** Load resource: $path")
+    logger.debug(s"*** load resource: $path")
     Using( Source.fromInputStream(getClass.getResourceAsStream(path), utf8) ) { 
-      source => source.mkString
-    }.getOrElse(s"*** Failed to load: $path")
+      source => source.mkString.getBytes
+    }.getOrElse(Array.empty[Byte])
+
+  def toHeader(resource: String): (String, String) =
+    logger.debug(s"*** to header: ${resource.split('.')(1)}")
+    resource.split('.')(1) match
+      case "ico"  => contentType -> "image/x-icon"
+      case "png"  => contentType -> "image/png"
+      case "js"   => contentType -> "text/javascript"
+      case "html" => indexHtmlHeader
+      case _      => contentType -> "text/plain"
 
 class Router(dispatcher: Dispatcher) extends Routes with LazyLogging:
-  val indexHeaders = Seq("Content-Type" -> "text/html; charset=UTF-8")
-  val imageResourceHeaders = Seq("Content-Type" -> "image/*")
-  val textResourceHeaders = Seq("Content-Type" -> "text/*")
+  import Router._
 
   @cask.get("/")
-  def index() = Response(Router.html, 200, indexHeaders)
+  def index() = Response(indexHtml, 200, Seq(indexHtmlHeader))
 
   @cask.get(Router.prefix, subpath = true)
   def resources(request: Request) =
-    val contentType = request.remainingPathSegments.head
-    if contentType.contains("ico") || contentType.contains("png") then
-      Response(Router.loadResource(contentType), 200, imageResourceHeaders)
-    else
-      Response(Router.loadResource(contentType), 200, textResourceHeaders)
+    val resource = request.remainingPathSegments.head
+    val content = loadResource(resource)
+    val headers = Seq(toHeader(resource))
+    Response(content, 200, headers)
 
   @cask.post("/command")
   def command(request: Request) =
