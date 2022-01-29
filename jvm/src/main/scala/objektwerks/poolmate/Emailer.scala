275da0e97809d4bd8f08ca107832f555b5d8b39d
objektwerks <objektwerks@runbox.com>
2022-01-29 11:44:07 -0500

    edited emailer logging

--- jvm/src/main/scala/objektwerks/poolmate/Emailer.scala
+++ jvm/src/main/scala/objektwerks/poolmate/Emailer.scala
@@ -87,16 +87,16 @@ final class Emailer(conf: Config,
           session.open()
           if session.isConnected then
             val messages = session.receiveEmailAndMarkSeen( filter.flag(Flags.Flag.SEEN, false) )
-            logger.info("*** Emailer receiveEmailAndMarkSeen messages: {}", messages.size)
+            logger.info("*** Emailer received email and mark-seen messages: {}", messages.size)
             store.listEmails.foreach { email =>
               messages.foreach { message =>
                 logger.info("*** Emailer subject {}", message.subject())
                 logger.info("*** Emailer message id: {}, email id: {}", message.messageId, email.id)
                 if message.subject != subject && message.messageId() == email.id then
                   store.processedEmail( email.copy(processed = true) )
-                  logger.info("*** Emailer [invalid] processedEmail: {}", email.id)
+                  logger.warn("*** Emailer [invalid] processed email: {}", email.id)
                   store.removeAccount(email.license)
-                  logger.info("*** Emailer removeAccount: {}", email.license)
+                  logger.warn("*** Emailer removed account: {}", email.license)
                 else if message.messageId() == email.id then
                   store.processedEmail( email.copy(processed = true, valid = true) )
                   logger.info("*** Emailer email processed and valid: {}", email)
