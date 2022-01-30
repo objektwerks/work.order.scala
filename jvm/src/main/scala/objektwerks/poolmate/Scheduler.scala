3907129ce56f0b3203bdac983613d6c38192ab4e
objektwerks <objektwerks@runbox.com>
2022-01-30 08:58:48 -0500

    refactored server for email processor

--- jvm/src/main/scala/objektwerks/poolmate/Scheduler.scala
+++ jvm/src/main/scala/objektwerks/poolmate/Scheduler.scala
@@ -3,13 +3,13 @@ package objektwerks.poolmate
 import java.util.concurrent._
 
 object Scheduler:
-  val initialDelay = 1L
-  val period = 11L
+  val initialDelay = 9L
+  val period = 30L
 
-final class Scheduler(emailSender: EmailSender):
+final class Scheduler(emailProcessor: EmailProcessor):
   import Scheduler.*
   
   val executor = new ScheduledThreadPoolExecutor(1)
-  val task = emailSender.send
+  val task = emailProcessor.process()
   val scheduler = executor.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.SECONDS)
   scheduler.cancel(false)
