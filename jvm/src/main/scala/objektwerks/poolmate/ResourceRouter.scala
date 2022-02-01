dfe52ee793812ba64bcb7f1e8d13af09512e4214
objektwerks <objektwerks@runbox.com>
2022-02-01 07:53:19 -0500

    renamed router to resource router

--- jvm/src/main/scala/objektwerks/poolmate/ResourceRouter.scala
+++ jvm/src/main/scala/objektwerks/poolmate/ResourceRouter.scala
@@ -4,11 +4,7 @@ import cask.main.Routes
 import cask.model.{Request, Response}
 import com.typesafe.scalalogging.LazyLogging
 
-import Serializers.given
-
-import upickle.default.{read, write}
-
-final class Router(dispatcher: Dispatcher, store: Store) extends Routes with LazyLogging with Resources("/public/"):
+final class ResourceRouter(dispatcher: Dispatcher) extends Routes with LazyLogging with Resources("/public/"):
   @cask.get("/")
   def index() = Response(indexHtml, 200, Seq(indexHtmlHeader))
 
@@ -18,18 +14,5 @@ final class Router(dispatcher: Dispatcher, store: Store) extends Routes with Laz
     val headers = Seq(toHeader(resource))
     if isImage(resource) then Response(loadImage(resource), 200, headers)
     else Response(loadResource(resource), 200, headers)
-
-  @cask.post("/command")
-  def command(request: Request) =
-    val command = read[Command](request.text())
-    logger.debug(s"*** Command: $command")
-
-    val event = dispatcher.dispatch(command)
-    logger.debug(s"*** Event: $event")
-    event match {
-      case fault: Fault => store.addFault(fault)
-      case _ =>
-    }
-    write[Event](event)
 
   initialize()
