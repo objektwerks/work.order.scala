2d71738476e9495a431d1ef94f47df9348eaa6d1
objektwerks <objektwerks@runbox.com>
2022-02-01 07:59:13 -0500

    added page

---
+++ js/src/main/scala/objektwerks/poolmate/Page.scala
@@ -0,0 +1,9 @@
+package objektwerks.poolmate
+
+import com.raquo.laminar.api.L.*
+
+sealed trait Page:
+  val title = "Poolmate"
+case object IndexPage extends Page
+case object RegisterPage extends Page
+case object LoginPage extends Page
