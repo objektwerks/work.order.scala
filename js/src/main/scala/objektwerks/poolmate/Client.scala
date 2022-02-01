cba82b8e90405c28ab544ea2a13376b5aed7bf62
objektwerks <objektwerks@runbox.com>
2022-02-01 08:09:04 -0500

    connected client to page router

--- js/src/main/scala/objektwerks/poolmate/Client.scala
+++ js/src/main/scala/objektwerks/poolmate/Client.scala
@@ -11,4 +11,7 @@ import scala.scalajs.js.annotation.JSExportTopLevel
 @JSExportTopLevel("Client")
 class Client(apiUrl: String) extends js.Object:
   log(s"api url: $apiUrl")
-  render(document.getElementById("content"), div())
+  render(
+    container = document.getElementById("container"),
+    rootNode = div( child <-- PageRouter.splitter.$view )
+  )
