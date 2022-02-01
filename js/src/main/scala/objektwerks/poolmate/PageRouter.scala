53716783684721cacd74c61afe5a64e604c50b8a
objektwerks <objektwerks@runbox.com>
2022-02-01 08:06:51 -0500

    added page router

---
+++ js/src/main/scala/objektwerks/poolmate/PageRouter.scala
@@ -0,0 +1,34 @@
+package objektwerks.poolmate
+
+import com.raquo.laminar.api.L
+import com.raquo.laminar.api.L.*
+import com.raquo.waypoint.*
+
+import upickle.default.*
+
+import org.scalajs.dom
+
+import Serializers.given
+
+object PageRouter:
+  val routes = List(
+    Route.static(IndexPage, root / endOfSegments),
+    Route.static(RegisterPage, root / "register" / endOfSegments),
+    Route.static(LoginPage, root / "login" / endOfSegments),
+  )
+
+  val router = new com.raquo.waypoint.Router[Page](
+    routes = routes,
+    serializePage = page => write(page)(pageRW),
+    deserializePage = pageAsString => read(pageAsString)(pageRW),
+    getPageTitle = _.title,
+  )(
+    $popStateEvent = L.windowEvents.onPopState,
+    owner = L.unsafeWindowOwner
+  )
+
+  val splitter = SplitRender[Page, HtmlElement](router.$currentPage)
+    .collectStatic(IndexPage) { IndexView() }
+    .collectStatic(RegisterPage) { RegisterView(Store.emailAddress) }
+    .collectStatic(LoginPage) { LoginView(Store.emailAddress, Store.pin) }
+    .collectStatic(TasksPage) { TasksView(Store.tasks) }
