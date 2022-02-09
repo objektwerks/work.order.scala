cf47a43854ae1b78690dc6938965f5f47e85e1e2
objektwerks <objektwerks@runbox.com>
2022-02-09 07:27:51 -0500

    added btn to components

--- js/src/main/scala/objektwerks/poolmate/Components.scala
+++ js/src/main/scala/objektwerks/poolmate/Components.scala
@@ -1,3 +1,7 @@
 package objektwerks.poolmate
 
+import com.raquo.laminar.api.L.*
+
 object Components:
+  def btn(label: String): HtmlElement =
+    button(cls("w3-button w3-round-xxlarge w3-light-grey w3-text-indigo"), label)
