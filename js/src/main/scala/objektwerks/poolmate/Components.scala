a0051863d73d0488a485c7360b4ebaf6c15c51e6
objektwerks <objektwerks@runbox.com>
2022-02-10 07:24:23 -0500

    added lbl component to components

--- js/src/main/scala/objektwerks/poolmate/Components.scala
+++ js/src/main/scala/objektwerks/poolmate/Components.scala
@@ -3,5 +3,8 @@ package objektwerks.poolmate
 import com.raquo.laminar.api.L.*
 
 object Components:
-  def btn(label: String): HtmlElement =
-    button(cls("w3-button w3-round-xxlarge w3-light-grey w3-text-indigo"), label)
+  def btn(text: String): HtmlElement =
+    button(cls("w3-button w3-round-xxlarge w3-light-grey w3-text-indigo"), text)
+
+  def lbl(text: String): HtmlElement =
+    label(cls("w3-left-align w3-text-indigo"), text)
