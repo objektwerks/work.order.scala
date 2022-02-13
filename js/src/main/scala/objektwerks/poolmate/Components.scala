24a91dbc75dcaa06cf9e94174c1866c99f319d58
objektwerks <objektwerks@runbox.com>
2022-02-13 08:56:50 -0500

    add int to components

--- js/src/main/scala/objektwerks/poolmate/Components.scala
+++ js/src/main/scala/objektwerks/poolmate/Components.scala
@@ -26,3 +26,5 @@ object Components:
   def err(errBus: EventBus[String]): Div = div(cls("w3-border-white w3-text-red"), child.text <-- errBus.events)
 
   def frm(elms: HtmlElement*): FormElement = form(cls("w3-container"), elms)
+
+  def int(): Input = input(cls(textCss), typ("number"), pattern("\\d*"), stepAttr("1"), required(true))
