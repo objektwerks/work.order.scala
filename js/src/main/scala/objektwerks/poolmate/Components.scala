3cbe2f283c4be322e9e8fa87ab97efb2e0395fbc
objektwerks <objektwerks@runbox.com>
2022-02-13 08:46:36 -0500

    added readonly rotext to components

--- js/src/main/scala/objektwerks/poolmate/Components.scala
+++ js/src/main/scala/objektwerks/poolmate/Components.scala
@@ -21,8 +21,14 @@ object Components:
   def hdr(text: String): HtmlElement =
     h5(cls("w3-light-grey w3-text-indigo"), text)
 
-  def txt: HtmlElement =
+  def txt: Input =
     input(cls("w3-input w3-hover-light-gray w3-text-indigo"))
+
+  def rotext: Input =
+    input(
+      cls("w3-input w3-hover-light-gray w3-text-indigo"),
+      readOnly(true)
+    )
 
   def err(errBus: EventBus[String]): HtmlElement =
     div(cls("w3-border-white w3-text-red"), child.text <-- errBus.events)
