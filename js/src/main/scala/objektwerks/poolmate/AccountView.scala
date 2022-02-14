a8164c6c42953a2c78196786c0f14c56682ebb3c
objektwerks <objektwerks@runbox.com>
2022-02-14 09:01:40 -0500

    added menu to account view

--- js/src/main/scala/objektwerks/poolmate/AccountView.scala
+++ js/src/main/scala/objektwerks/poolmate/AccountView.scala
@@ -2,5 +2,19 @@ package objektwerks.poolmate
 
 import com.raquo.laminar.api.L.*
 
+import org.scalajs.dom.console.log
+
+import Components.*
+
 object AccountView:
-  def apply(account: Var[Account]): HtmlElement = div()
+  def apply(account: Var[Account]): HtmlElement =
+    div(
+      bar(
+        btn("Pools").amend {
+          onClick --> { _ =>
+            log("Pools onClick")
+            PageRouter.router.pushState(PoolsPage)
+          }
+        }      
+      )      
+    )
