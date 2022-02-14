1a9566b28e1f42fc7c03c5b25f6f91bdbab6cb4d
objektwerks <objektwerks@runbox.com>
2022-02-14 09:00:14 -0500

    added menu to pools view

--- js/src/main/scala/objektwerks/poolmate/PoolsView.scala
+++ js/src/main/scala/objektwerks/poolmate/PoolsView.scala
@@ -2,5 +2,19 @@ package objektwerks.poolmate
 
 import com.raquo.laminar.api.L.*
 
+import org.scalajs.dom.console.log
+
+import Components.*
+
 object PoolsView:
-  def apply(pools: Var[Seq[Pool]], selectedPool: Var[Pool]): HtmlElement = div()
+  def apply(pools: Var[Seq[Pool]], selectedPool: Var[Pool]): HtmlElement =
+    div(
+      bar(
+        btn("Account").amend {
+          onClick --> { _ =>
+            log("Account onClick")
+            PageRouter.router.pushState(PoolsPage)
+          }
+        }      
+      )      
+    )
