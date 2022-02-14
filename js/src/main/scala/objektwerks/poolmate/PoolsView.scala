e1af800959160b9677dc3d3803239dfffba044d6
objektwerks <objektwerks@runbox.com>
2022-02-14 08:50:57 -0500

    added pools and selected pool to pools view

--- js/src/main/scala/objektwerks/poolmate/PoolsView.scala
+++ js/src/main/scala/objektwerks/poolmate/PoolsView.scala
@@ -3,4 +3,4 @@ package objektwerks.poolmate
 import com.raquo.laminar.api.L.*
 
 object PoolsView:
-  def apply(): HtmlElement = div()
+  def apply(pools: Var[Seq[Pool]], selectedPool: Var[Pool]): HtmlElement = div()
