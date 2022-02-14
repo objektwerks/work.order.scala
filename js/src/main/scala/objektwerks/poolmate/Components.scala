698732fb5250591e3bc6abd3e376cd67f4830271
objektwerks <objektwerks@runbox.com>
2022-02-14 08:13:01 -0500

    added rodbl

--- js/src/main/scala/objektwerks/poolmate/Components.scala
+++ js/src/main/scala/objektwerks/poolmate/Components.scala
@@ -31,6 +31,8 @@ object Components:
 
   def int: Input = input(cls(textCss), typ("number"), pattern("\\d*"), stepAttr("1"), required(true))
 
-  def roint: Input = input(cls(textCss), typ("number"), readOnly(true))
+  def roint: Input = input(cls(textCss), typ("number"), pattern("\\d*"), readOnly(true))
 
   def dbl: Input = input(cls(textCss), typ("number"), pattern("[0-9]+([.,][0-9]+)?"), stepAttr("0.01"), required(true))
+
+  def rodbl: Input = input(cls(textCss), typ("number"), pattern("[0-9]+([.,][0-9]+)?"), readOnly(true))
