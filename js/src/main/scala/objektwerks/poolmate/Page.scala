6afc0aad9ca23c26046dda2282bdbc048ff38e91
objektwerks <objektwerks@runbox.com>
2022-02-14 08:24:22 -0500

    removed account page from page

--- js/src/main/scala/objektwerks/poolmate/Page.scala
+++ js/src/main/scala/objektwerks/poolmate/Page.scala
@@ -7,5 +7,4 @@ sealed trait Page:
 case object IndexPage extends Page
 case object RegisterPage extends Page
 case object LoginPage extends Page
-case object AccountPage extends Page
 case object PoolsPage extends Page
