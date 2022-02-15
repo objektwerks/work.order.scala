73135a47f70c7b255464f1c782490a04ead18431
objektwerks <objektwerks@runbox.com>
2022-02-15 18:51:20 -0500

    reformatted split code in components

--- js/src/main/scala/objektwerks/poolmate/Components.scala
+++ js/src/main/scala/objektwerks/poolmate/Components.scala
@@ -36,7 +36,7 @@ object Components:
   def item(strSignal: Signal[String]): Li = li(cls("w3-text-indigo w3-display-container"), child.text <-- strSignal)
 
   def split[E <: Entity](entities: Var[Seq[E]], toEntityPage: Long => EntityPage): Signal[Seq[Li]] =
-    entities.signal.split(_.id)((id, _, entitySignal) =>
+    entities.signal.split(_.id)( (id, _, entitySignal) =>
       item( entitySignal.map(_.display) ).amend {
         onClick --> { _ =>
           entities.now().find(_.id == id).foreach { entity =>
