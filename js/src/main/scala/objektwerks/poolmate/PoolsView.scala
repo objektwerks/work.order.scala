6400adafbd717edd3bcb221828ac6256a87b0788
objektwerks <objektwerks@runbox.com>
2022-02-15 09:08:50 -0500

    refactored pools view split method

--- js/src/main/scala/objektwerks/poolmate/PoolsView.scala
+++ js/src/main/scala/objektwerks/poolmate/PoolsView.scala
@@ -8,16 +8,17 @@ import Components.*
 
 object PoolsView:
   def apply(pools: Var[Seq[Pool]], selectedPool: Var[Pool]): HtmlElement =
-    def split: Signal[Seq[Li]] = pools.signal.split(_.id)( (id, _, poolSignal) =>
-      item(poolSignal.map(_.name)).amend {
-        onClick --> { _ =>
-          pools.now().find(_.id == id).foreach { pool =>
-            selectedPool.set(pool)
-            //Todo show pool view
+    def split(poolsSignal: StrictSignal[Seq[Pool]]): Signal[Seq[Li]] =
+      poolsSignal.split(_.id)( (id, _, poolSignal) =>
+        item(poolSignal.map(_.name)).amend {
+          onClick --> { _ =>
+            pools.now().find(_.id == id).foreach { pool =>
+              selectedPool.set(pool)
+              //Todo show pool view
+            }
           }
         }
-      }
-    )
+      )
     div(
       bar(
         btn("Account").amend {
@@ -29,6 +30,6 @@ object PoolsView:
       ),
       div(
         hdr("Pools"),
-        list(split)
+        list(split(pools.signal))
       )
     )
