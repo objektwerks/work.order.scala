d3ed542899e99ea266860521308bd2bd71ebfefd
objektwerks <objektwerks@runbox.com>
2022-02-15 03:23:44 -0500

    reordered components code

--- js/src/main/scala/objektwerks/poolmate/Components.scala
+++ js/src/main/scala/objektwerks/poolmate/Components.scala
@@ -12,23 +12,23 @@ object Components:
   def btn(text: String): Button = button(cls("w3-button w3-round-xxlarge w3-light-grey w3-text-indigo"), text)
 
   def rbtn(text: String): HtmlElement = button(cls("w3-button w3-round-xxlarge w3-light-grey w3-text-indigo w3-right"), text)
-
-  def email: Input = input(cls(textCss), typ("email"), minLength(3), placeholder("address@email.com"), required(true))
-
-  def pin: Input = input(cls(textCss), typ("text"), minLength(9), maxLength(9), required(true), placeholder("a1b2c3d4e"))
-
+  
   def lbl(text: String): Label = label(cls("w3-left-align w3-text-indigo"), text)
-
-  def hdr(text: String): HtmlElement = h5(cls("w3-light-grey w3-text-indigo"), text)
 
   def txt: Input = input(cls(textCss), required(true))
 
   def rotxt: Input = input(cls(textCss), readOnly(true))
 
-  def err(errBus: EventBus[String]): Div = div(cls("w3-border-white w3-text-red"), child.text <-- errBus.events)
+  def email: Input = input(cls(textCss), typ("email"), minLength(3), placeholder("address@email.com"), required(true))
 
-  def frm(elms: HtmlElement*): FormElement = form(cls("w3-container"), elms)
+  def pin: Input = input(cls(textCss), typ("text"), minLength(9), maxLength(9), required(true), placeholder("a1b2c3d4e"))
 
   def int: Input = input(cls(textCss), typ("number"), pattern("\\d*"), stepAttr("1"), required(true))
 
   def dbl: Input = input(cls(textCss), typ("number"), pattern("[0-9]+([.,][0-9]+)?"), stepAttr("0.01"), required(true))
+
+  def hdr(text: String): HtmlElement = h5(cls("w3-light-grey w3-text-indigo"), text)
+
+  def err(errBus: EventBus[String]): Div = div(cls("w3-border-white w3-text-red"), child.text <-- errBus.events)
+
+  def frm(elms: HtmlElement*): FormElement = form(cls("w3-container"), elms)
