690c81a29ae59ebe8dc6211ce8a2f899022da67d
objektwerks <objektwerks@runbox.com>
2022-02-16 06:32:42 -0500

    removed pools model

--- js/src/main/scala/objektwerks/poolmate/Model.scala
+++ js/src/main/scala/objektwerks/poolmate/Model.scala
@@ -8,25 +8,6 @@ object Model:
   val account = Var(Account.empty)
   val pools = Entities[Pool](Pool(), Var(Seq.empty[Pool]), Var(Pool()))
 
-/*
-class Pools(val emptyPool: Pool):
-  val poolsVar = Var(Seq.empty[Pool])
-  val poolVar = Var(emptyPool)
-  def set(pools: Seq[Pool]): Pools =
-    poolsVar.set(pools)
-    this
-  def set(id: Long): Pools =
-    poolVar.set(poolsVar.now().find(_.id == id).getOrElse(emptyPool))
-    this
-  def update(pool: Pool): Unit =
-    poolsVar.update( _.map( p =>
-      if p.id == pool.id then
-        poolVar.set(pool)
-        pool
-      else p
-    ))
-*/
-
 case class Entities[E <: Entity](emptyEntity: E,
                                  entitiesVar: Var[Seq[E]],
                                  entityVar: Var[E]):
