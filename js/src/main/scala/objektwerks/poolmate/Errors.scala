ffca7dab87a62f0f3aa33a0eb1e7c3409c407530
objektwerks <objektwerks@runbox.com>
2022-02-17 07:00:40 -0500

    added non zero error to errors

--- js/src/main/scala/objektwerks/poolmate/Errors.scala
+++ js/src/main/scala/objektwerks/poolmate/Errors.scala
@@ -4,3 +4,4 @@ object Errors:
   val emailAddressError = "Enter a valid email address."
   val pinError = "Enter a valid 9-character pin."
   val nonEmptyError = "Enter 1 character or more."
+  val nonZeroError = "Enter 1 or greater."
