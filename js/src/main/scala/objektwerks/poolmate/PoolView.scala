2e86d822611fcc3e935196870ae82b24dd4958cc
objektwerks <objektwerks@runbox.com>
2022-02-17 08:23:07 -0500

    renamed id validator methods

--- js/src/main/scala/objektwerks/poolmate/PoolView.scala
+++ js/src/main/scala/objektwerks/poolmate/PoolView.scala
@@ -60,14 +60,14 @@ object PoolView:
       ),
       cbar(
         btn("Add").amend {
-          disabled <-- model.entityVar.signal.map { pool => !(pool.id.isIdZero && pool.isValid) }
+          disabled <-- model.entityVar.signal.map { pool => !(pool.id.isZero && pool.isValid) }
           onClick --> { _ =>
             log(s"Add onClick")
             PageRouter.router.pushState(PoolsPage)
           }
         },
         btn("Update").amend {
-          disabled <-- model.entityVar.signal.map { pool => !(pool.id.isIdGreaterThanZero && pool.isValid) }
+          disabled <-- model.entityVar.signal.map { pool => !(pool.id.isGreaterThanZero && pool.isValid) }
           onClick --> { _ =>
             log(s"Update onClick")
             PageRouter.router.pushState(PoolsPage)
