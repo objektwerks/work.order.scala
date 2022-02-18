c5434a841cf0354c1011c102269d6e0728d2a843
objektwerks <objektwerks@runbox.com>
2022-02-18 09:36:20 -0500

    edited client url

--- js/src/main/scala/poolmate/Client.scala
+++ js/src/main/scala/poolmate/Client.scala
@@ -9,8 +9,8 @@ import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 @JSExportTopLevel("Client")
-class Client(apiUrl: String) extends js.Object:
-  log(s"api url: $apiUrl")
+class Client(rootUrl: String) extends js.Object:
+  log(s"root url: $rootUrl")
   render(
     container = document.getElementById("container"),
     rootNode = div( child <-- PageRouter.splitter.$view )
