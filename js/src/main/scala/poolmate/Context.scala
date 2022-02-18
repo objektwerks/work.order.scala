57182493d78af827209c5361da3909dfe028443a
objektwerks <objektwerks@runbox.com>
2022-02-18 10:05:53 -0500

    added global js Context object

---
+++ js/src/main/scala/poolmate/Context.scala
@@ -0,0 +1,9 @@
+package poolmate
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobalScope
+
+@js.native
+@JSGlobalScope
+object Context extends js.Object:
+  var rootUrl: String = js.native
