2bc6644228e5ae6907c9cd6b4ddeb4c9e4abde54
objektwerks <objektwerks@runbox.com>
2022-02-18 10:16:35 -0500

    converted global js object context to scala object

--- js/src/main/scala/poolmate/Context.scala
+++ js/src/main/scala/poolmate/Context.scala
@@ -1,11 +1,6 @@
 package poolmate
 
-import scala.scalajs.js
-import scala.scalajs.js.annotation.JSGlobalScope
-
-@js.native
-@JSGlobalScope
-object Context extends js.Object:
-  var rootUrl: String = js.native
-  var nowUrl: String = js.native
-  var commandUrl: String = js.native
+object Context:
+  var rootUrl: String = ""
+  var nowUrl: String = ""
+  var commandUrl: String = ""
