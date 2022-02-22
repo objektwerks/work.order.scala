7b9c67128737ae4ca81718626bbcd1de5e9210f7
objektwerks <objektwerks@runbox.com>
2022-02-22 07:09:49 -0500

    rearranged client imports

--- js/src/main/scala/poolmate/Client.scala
+++ js/src/main/scala/poolmate/Client.scala
@@ -5,9 +5,9 @@ import com.raquo.laminar.api.L._
 import org.scalajs.dom._
 import org.scalajs.dom.console.log
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExportTopLevel
-import scala.concurrent.ExecutionContext.Implicits.global
 
 @JSExportTopLevel("Client")
 class Client(rootUrl: String) extends js.Object:
