ff7092c7f77cc42b18c91c3659d1db844bd08ec1
objektwerks <objektwerks@runbox.com>
2022-02-27 08:26:28 -0500

    edited log statement

--- js/src/main/scala/poolmate/PoolView.scala
+++ js/src/main/scala/poolmate/PoolView.scala
@@ -62,14 +62,14 @@ object PoolView:
         btn("Add").amend {
           disabled <-- model.selectedEntityVar.signal.map { pool => !(pool.id.isZero && pool.isValid) }
           onClick --> { _ =>
-            log(s"Add onClick")
+            log(s"Pool Add onClick")
             PageRouter.router.pushState(PoolsPage)
           }
         },
         btn("Update").amend {
           disabled <-- model.selectedEntityVar.signal.map { pool => !(pool.id.isGreaterThanZero && pool.isValid) }
           onClick --> { _ =>
-            log(s"Update onClick")
+            log(s"Pool Update onClick")
             PageRouter.router.pushState(PoolsPage)
           }
         }
