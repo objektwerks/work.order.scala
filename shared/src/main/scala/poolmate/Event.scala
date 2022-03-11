301d6b53b9462cd8e35bb2bb1941e66ceed9e02c
objektwerks <objektwerks@runbox.com>
2022-03-11 14:05:48 -0500

    added surface events

--- shared/src/main/scala/poolmate/Event.scala
+++ shared/src/main/scala/poolmate/Event.scala
@@ -18,6 +18,9 @@ final case class Updated() extends Event
 final case class PoolsListed(pools: Seq[Pool]) extends Event
 final case class PoolAdded(pool: Pool) extends Event
 
+final case class SurfacesListed(surfaces: Seq[Surface]) extends Event
+final case class SurfaceAdded(surface: Surface) extends Event
+
 final case class Fault(dateOf: Int = DateTime.currentDate,
                        timeOf: Int = DateTime.currentTime,
                        nanoOf: Int = DateTime.nano,
