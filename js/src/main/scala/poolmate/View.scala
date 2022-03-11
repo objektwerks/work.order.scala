13290dea099a9f65effb94e50315d92421a36e23
objektwerks <objektwerks@runbox.com>
2022-03-11 07:24:46 -0500

    added call method to view

--- js/src/main/scala/poolmate/View.scala
+++ js/src/main/scala/poolmate/View.scala
@@ -1,4 +1,6 @@
 package poolmate
 
 trait View:
+  def call(command: Command, handler: (event: Either[Fault, Event]) => Unit): Unit = Proxy.call(command, handler)
+  
   def route(page: Page): Unit = PageRouter.router.pushState(page)
