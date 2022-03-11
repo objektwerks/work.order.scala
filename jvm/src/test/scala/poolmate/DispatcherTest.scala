5e5c690d28a90dfb2122aea9f0c57493bdb9414c
objektwerks <objektwerks@runbox.com>
2022-03-11 17:32:00 -0500

    >edited timer setting events in dispatcher test

--- jvm/src/test/scala/poolmate/DispatcherTest.scala
+++ jvm/src/test/scala/poolmate/DispatcherTest.scala
@@ -204,7 +204,7 @@ class DispatcherTest extends AnyFunSuite with Matchers with LazyLogging:
   def testAddTimerSetting(dispatcher: Dispatcher, pool: Pool, timerSetting: TimerSetting): TimerSetting =
     val add = AddTimerSetting(pool.license, timerSetting)
     dispatcher.dispatch(add) match
-      case Added(timerSetting: TimerSetting) =>
+      case TimerSettingAdded(timerSetting: TimerSetting) =>
         timerSetting.id > 0 shouldBe true
         timerSetting
       case event: Event => logger.error(event.toString); fail()
@@ -212,7 +212,7 @@ class DispatcherTest extends AnyFunSuite with Matchers with LazyLogging:
   def testListTimerSettings(dispatcher: Dispatcher, pool: Pool, timer: Timer): Unit =
     val list = ListTimerSettings(pool.license, timer.id)
     dispatcher.dispatch(list) match
-      case Listed(timerSettings) => timerSettings.size shouldBe 1
+      case TimerSettingsListed(timerSettings) => timerSettings.size shouldBe 1
       case event: Event => logger.error(event.toString); fail()
 
   def testUpdateTimerSetting(dispatcher: Dispatcher, pool: Pool, timerSetting: TimerSetting): Unit =
