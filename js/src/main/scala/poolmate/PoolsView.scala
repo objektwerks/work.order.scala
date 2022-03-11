a6f580223cb1e275cfbae1c3e2e43339af07f0aa
objektwerks <objektwerks@runbox.com>
2022-03-11 08:12:10 -0500

    added account var to pools view

--- js/src/main/scala/poolmate/PoolsView.scala
+++ js/src/main/scala/poolmate/PoolsView.scala
@@ -7,14 +7,14 @@ import org.scalajs.dom.console.log
 import Components.*
 
 object PoolsView extends View:
-  def apply(model: Model[Pool]): HtmlElement =
+  def apply(model: Model[Pool], accountVar: Var[Account]): HtmlElement =
     def handler(event: Either[Fault, Event]): Unit =
       event match
         case Right(event) =>
           event match
             case PoolsListed(pools: Seq[Pool]) =>
               clearErrors()
-              
+
             case _ =>
         case Left(fault) => errorBus.emit(s"List pools failed: ${fault.cause}")
 
@@ -28,7 +28,7 @@ object PoolsView extends View:
         }      
       ),
       div(
-        onLoad --> { _ => call(ListPools(""), handler) },
+        onLoad --> { _ => call(ListPools(accountVar.now().license), handler) },
         hdr("Pools"),
         list(
           split(model.entitiesVar, (id: Long) => PoolPage(id))
