1f02bee603d75087562a6defe6ed4fc5b928bfd0
objektwerks <objektwerks@runbox.com>
2022-03-11 14:11:53 -0500

    removed listed and added events

--- shared/src/main/scala/poolmate/Event.scala
+++ shared/src/main/scala/poolmate/Event.scala
@@ -11,8 +11,6 @@ final case class LoggedIn(account: Account) extends Event
 final case class Deactivated(account: Account) extends Event
 final case class Reactivated(account: Account) extends Event
 
-final case class Listed(entities: Seq[Entity]) extends Event
-final case class Added(entity: Entity) extends Event
 final case class Updated() extends Event
 
 final case class PoolsListed(pools: Seq[Pool]) extends Event
