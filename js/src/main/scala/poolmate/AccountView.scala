529723c09fe4c7c7f03181a187b3707784547b39
objektwerks <objektwerks@runbox.com>
2022-03-14 09:50:22 -0400

    added home menu item to account view

--- js/src/main/scala/poolmate/AccountView.scala
+++ js/src/main/scala/poolmate/AccountView.scala
@@ -32,43 +32,53 @@ object AccountView extends View:
         case Left(fault) => errorBus.emit(s"Reactivate failed: ${fault.cause}")
 
     div(
-      hdr("Account"),
-      lbl("License"),
-      rotxt.amend {
-        value <-- accountVar.signal.map(_.license)
-      },
-      lbl("Email Address"),
-      rotxt.amend {
-        value <-- accountVar.signal.map(_.emailAddress)
-      },
-      lbl("Pin"),
-      rotxt.amend {
-        value <-- accountVar.signal.map(_.pin)
-      },
-      lbl("Activated"),
-      rotxt.amend {
-        value <-- accountVar.signal.map(_.activated.toString)
-      },
-      lbl("Deactivated"),
-      rotxt.amend {
-        value <-- accountVar.signal.map(_.deactivated.toString)
-      },
-      cbar(
-        btn("Deactivate").amend {
-          disabled <-- accountVar.signal.map { account => account.isDeactivated }
+      bar(
+        btn("Home").amend {
           onClick --> { _ =>
-            log("Account -> Deactivate onClick")
-            val command = Deactivate(accountVar.now().license)
-            call(command, deactivateHandler)
-          }
-        },
-        btn("Reactivate").amend {
-          disabled <-- accountVar.signal.map { account => account.isActivated }
-          onClick --> { _ =>
-            log("Account -> Reactivate onClick")
-            val command = Reactivate(accountVar.now().license)
-            call(command, reactivateHandler)
+            log("Account -> Home onClick")
+            route(HomePage)
           }
         }      
+      ),
+      div(
+        hdr("Account"),
+        lbl("License"),
+        rotxt.amend {
+          value <-- accountVar.signal.map(_.license)
+        },
+        lbl("Email Address"),
+        rotxt.amend {
+          value <-- accountVar.signal.map(_.emailAddress)
+        },
+        lbl("Pin"),
+        rotxt.amend {
+          value <-- accountVar.signal.map(_.pin)
+        },
+        lbl("Activated"),
+        rotxt.amend {
+          value <-- accountVar.signal.map(_.activated.toString)
+        },
+        lbl("Deactivated"),
+        rotxt.amend {
+          value <-- accountVar.signal.map(_.deactivated.toString)
+        },
+        cbar(
+          btn("Deactivate").amend {
+            disabled <-- accountVar.signal.map { account => account.isDeactivated }
+            onClick --> { _ =>
+              log("Account -> Deactivate onClick")
+              val command = Deactivate(accountVar.now().license)
+              call(command, deactivateHandler)
+            }
+          },
+          btn("Reactivate").amend {
+            disabled <-- accountVar.signal.map { account => account.isActivated }
+            onClick --> { _ =>
+              log("Account -> Reactivate onClick")
+              val command = Reactivate(accountVar.now().license)
+              call(command, reactivateHandler)
+            }
+          }      
+        )
       )
     )
