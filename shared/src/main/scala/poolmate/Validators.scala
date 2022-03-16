a883f878925b3072e1d87c3da56a5f93c27f1fc3
objektwerks <objektwerks@runbox.com>
2022-03-16 05:19:20 -0400

    added is name validator

--- shared/src/main/scala/poolmate/Validators.scala
+++ shared/src/main/scala/poolmate/Validators.scala
@@ -5,6 +5,7 @@ object Validators:
     def isLicense: Boolean = if value.nonEmpty then value.length == 36 else false
     def isEmailAddress: Boolean = value.nonEmpty && value.length >= 3 && value.contains("@")
     def isPin: Boolean = value.length == 6
+    def isName: Boolean = value.length == 2
 
   extension (id: Long)
     def isZero: Boolean = id == 0
