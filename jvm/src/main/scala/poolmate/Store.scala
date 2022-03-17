704c9f2731a2fae52c9b2290d6acf0c2bcb71831
objektwerks <objektwerks@runbox.com>
2022-03-17 16:54:51 -0400

    refactored store add account

--- jvm/src/main/scala/poolmate/Store.scala
+++ jvm/src/main/scala/poolmate/Store.scala
@@ -37,7 +37,7 @@ final class Store(conf: Config, cache: Cache[String, String]) extends LazyLoggin
         .list()
     }
 
-  def addAccount(account: Account): Unit =
+  def addAccount(account: Account): Account =
     val id = DB localTx { implicit session =>
       sql"insert into account(license, email_address, pin, activated, deactivated) values(${account.license}, ${account.emailAddress}, ${account.pin}, ${account.activated}, ${account.deactivated})"
       .update()
