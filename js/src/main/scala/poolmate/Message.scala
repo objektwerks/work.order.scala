3959503adfba73ca946f8c13b3a5bd20301a0e91
objektwerks <objektwerks@runbox.com>
2022-03-18 19:18:14 -0400

    edited login message

--- js/src/main/scala/poolmate/Message.scala
+++ js/src/main/scala/poolmate/Message.scala
@@ -1,5 +1,5 @@
 package poolmate
 
 object Message:
-  val exploreMessage = "On success, you'll transition to the Login page. You must record your pin!"
+  val loginMessage = "Record your below pin to, going forward, login to your account."
   val pinMessage = "Keep your pin of 6 alphanumeric characters in a secure place!"
