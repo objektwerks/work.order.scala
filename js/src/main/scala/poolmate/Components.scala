fa83cdc3791675f1eebe1505561a4dd3dbf2ac4d
objektwerks <objektwerks@runbox.com>
2022-03-18 09:28:05 -0400

    added info to components

--- js/src/main/scala/poolmate/Components.scala
+++ js/src/main/scala/poolmate/Components.scala
@@ -27,6 +27,9 @@ object Components:
   def lbl(text: String): Label =
     label(cls("w3-left-align w3-text-indigo"), text)
 
+  def info(text: String): Label =
+    label(cls("w3-left-align w3-indigo w3-text-white"), text)
+
   def txt: Input =
     input(cls(inputCss), required(true))
 
