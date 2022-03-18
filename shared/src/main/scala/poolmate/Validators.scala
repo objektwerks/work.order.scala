2aae54d62a199a8aeafc87dd533259d5b5e93ca4
objektwerks <objektwerks@runbox.com>
2022-03-18 17:15:03 -0400

    removed email address validator

--- shared/src/main/scala/poolmate/Validators.scala
+++ shared/src/main/scala/poolmate/Validators.scala
@@ -3,7 +3,6 @@ package poolmate
 object Validators:
   extension (value: String)
     def isLicense: Boolean = if value.nonEmpty then value.length == 36 else false
-    def isEmailAddress: Boolean = value.nonEmpty && value.length >= 3 && value.contains("@")
     def isPin: Boolean = value.length == 6
     def isName: Boolean = value.length >= 2
 
