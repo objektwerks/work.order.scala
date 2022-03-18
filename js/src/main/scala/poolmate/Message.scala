e6970f2685e733b00b1cc7bce3df3aec1855b3a6
objektwerks <objektwerks@runbox.com>
2022-03-18 18:45:37 -0400

    edited register message

--- js/src/main/scala/poolmate/Message.scala
+++ js/src/main/scala/poolmate/Message.scala
@@ -1,5 +1,5 @@
 package poolmate
 
 object Message:
-  val registerMessage = "Upon success, you'll transition to the Login page. Record your pin!"
+  val registerMessage = "On success, you'll transition to the Login page. Record your pin!"
   val pinMessage = "Keep your pin of 6 alphanumeric characters in a secure place!"
