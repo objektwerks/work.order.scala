0b807320b9e24367693b9a7e9f7ffb90fd7cc2b8
objektwerks <objektwerks@runbox.com>
2022-03-20 11:02:22 -0400

    reordered events

--- shared/src/main/scala/poolmate/Event.scala
+++ shared/src/main/scala/poolmate/Event.scala
@@ -16,11 +16,20 @@ final case class Updated() extends Event
 final case class PoolsListed(pools: List[Pool]) extends Event
 final case class PoolAdded(pool: Pool) extends Event
 
-final case class SurfacesListed(surfaces: List[Surface]) extends Event
-final case class SurfaceAdded(surface: Surface) extends Event
+final case class MeasurementsListed(measurements: List[Measurement]) extends Event
+final case class MeasurementAdded(measurement: Measurement) extends Event
 
-final case class DecksListed(decks: List[Deck]) extends Event
-final case class DeckAdded(deck: Deck) extends Event
+final case class CleaningsListed(cleanings: List[Cleaning]) extends Event
+final case class CleaningAdded(cleaning: Cleaning) extends Event
+
+final case class ChemicalsListed(chemicals: List[Chemical]) extends Event
+final case class ChemicalAdded(chemical: Chemical) extends Event
+
+final case class SuppliesListed(supplies: List[Supply]) extends Event
+final case class SupplyAdded(supply: Supply) extends Event
+
+final case class RepairsListed(repairs: List[Repair]) extends Event
+final case class RepairAdded(repair: Repair) extends Event
 
 final case class PumpsListed(pumps: List[Pump]) extends Event
 final case class PumpAdded(pump: Pump) extends Event
@@ -37,20 +46,11 @@ final case class HeaterAdded(heater: Heater) extends Event
 final case class HeaterSettingsListed(heaterSettings: List[HeaterSetting]) extends Event
 final case class HeaterSettingAdded(heaterSetting: HeaterSetting) extends Event
 
-final case class MeasurementsListed(measurements: List[Measurement]) extends Event
-final case class MeasurementAdded(measurement: Measurement) extends Event
+final case class SurfacesListed(surfaces: List[Surface]) extends Event
+final case class SurfaceAdded(surface: Surface) extends Event
 
-final case class CleaningsListed(cleanings: List[Cleaning]) extends Event
-final case class CleaningAdded(cleaning: Cleaning) extends Event
-
-final case class ChemicalsListed(chemicals: List[Chemical]) extends Event
-final case class ChemicalAdded(chemical: Chemical) extends Event
-
-final case class SuppliesListed(supplies: List[Supply]) extends Event
-final case class SupplyAdded(supply: Supply) extends Event
-
-final case class RepairsListed(repairs: List[Repair]) extends Event
-final case class RepairAdded(repair: Repair) extends Event
+final case class DecksListed(decks: List[Deck]) extends Event
+final case class DeckAdded(deck: Deck) extends Event
 
 final case class Fault(dateOf: Int = DateTime.currentDate,
                        timeOf: Int = DateTime.currentTime,
