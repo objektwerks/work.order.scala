63face7bea78f3f98065245d8a53a462cd6d2cad
objektwerks <objektwerks@runbox.com>
2022-03-20 11:00:59 -0400

    reordered commands

--- shared/src/main/scala/poolmate/Command.scala
+++ shared/src/main/scala/poolmate/Command.scala
@@ -15,13 +15,25 @@ final case class ListPools(license: String) extends Command with License
 final case class AddPool(license: String, pool: Pool) extends Command with License
 final case class UpdatePool(license: String, pool: Pool) extends Command with License
 
-final case class ListSurfaces(license: String, poolId: Long) extends Command with License
-final case class AddSurface(license: String, surface: Surface) extends Command with License
-final case class UpdateSurface(license: String, surface: Surface) extends Command with License
+final case class ListMeasurements(license: String, poolId: Long) extends Command with License
+final case class AddMeasurement(license: String, measurement: Measurement) extends Command with License
+final case class UpdateMeasurement(license: String, measurement: Measurement) extends Command with License
 
-final case class ListDecks(license: String, poolId: Long) extends Command with License
-final case class AddDeck(license: String, deck: Deck) extends Command with License
-final case class UpdateDeck(license: String, deck: Deck) extends Command with License
+final case class ListCleanings(license: String, poolId: Long) extends Command with License
+final case class AddCleaning(license: String, cleaning: Cleaning) extends Command with License
+final case class UpdateCleaning(license: String, cleaning: Cleaning) extends Command with License
+
+final case class ListChemicals(license: String, poolId: Long) extends Command with License
+final case class AddChemical(license: String, chemical: Chemical) extends Command with License
+final case class UpdateChemical(license: String, chemical: Chemical) extends Command with License
+
+final case class ListSupplies(license: String, poolId: Long) extends Command with License
+final case class AddSupply(license: String, supply: Supply) extends Command with License
+final case class UpdateSupply(license: String, supply: Supply) extends Command with License
+
+final case class ListRepairs(license: String, poolId: Long) extends Command with License
+final case class AddRepair(license: String, repair: Repair) extends Command with License
+final case class UpdateRepair(license: String, repair: Repair) extends Command with License
 
 final case class ListPumps(license: String, poolId: Long) extends Command with License
 final case class AddPump(license: String, pump: Pump) extends Command with License
@@ -43,22 +55,10 @@ final case class ListHeaterSettings(license: String, heaterId: Long) extends Com
 final case class AddHeaterSetting(license: String, heaterSetting: HeaterSetting) extends Command with License
 final case class UpdateHeaterSetting(license: String, heaterSetting: HeaterSetting) extends Command with License
 
-final case class ListMeasurements(license: String, poolId: Long) extends Command with License
-final case class AddMeasurement(license: String, measurement: Measurement) extends Command with License
-final case class UpdateMeasurement(license: String, measurement: Measurement) extends Command with License
+final case class ListSurfaces(license: String, poolId: Long) extends Command with License
+final case class AddSurface(license: String, surface: Surface) extends Command with License
+final case class UpdateSurface(license: String, surface: Surface) extends Command with License
 
-final case class ListCleanings(license: String, poolId: Long) extends Command with License
-final case class AddCleaning(license: String, cleaning: Cleaning) extends Command with License
-final case class UpdateCleaning(license: String, cleaning: Cleaning) extends Command with License
-
-final case class ListChemicals(license: String, poolId: Long) extends Command with License
-final case class AddChemical(license: String, chemical: Chemical) extends Command with License
-final case class UpdateChemical(license: String, chemical: Chemical) extends Command with License
-
-final case class ListSupplies(license: String, poolId: Long) extends Command with License
-final case class AddSupply(license: String, supply: Supply) extends Command with License
-final case class UpdateSupply(license: String, supply: Supply) extends Command with License
-
-final case class ListRepairs(license: String, poolId: Long) extends Command with License
-final case class AddRepair(license: String, repair: Repair) extends Command with License
-final case class UpdateRepair(license: String, repair: Repair) extends Command with License
+final case class ListDecks(license: String, poolId: Long) extends Command with License
+final case class AddDeck(license: String, deck: Deck) extends Command with License
+final case class UpdateDeck(license: String, deck: Deck) extends Command with License
