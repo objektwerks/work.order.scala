d59cc3422dc3b0a6a7ba08ed90d7559c55eb2e63
objektwerks <objektwerks@runbox.com>
2022-03-20 15:28:27 -0400

    refactored authorizer to handle explore and enter; router now handles unauthorized

--- jvm/src/main/scala/poolmate/Authorizer.scala
+++ jvm/src/main/scala/poolmate/Authorizer.scala
@@ -1,7 +1,9 @@
 package poolmate
 
+import Validators.*
+
 final class Authorizer(service: Service):
   def authorize(command: Command): Event =
     command match
       case license: License => service.authorize(license.license)
-      case _ => Authorized("")
+      case Explore() | Enter(_) => Authorized("")
