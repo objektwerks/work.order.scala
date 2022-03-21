135b8e321df47d6bd3b6d22ed50dd529185d7beb
objektwerks <objektwerks@runbox.com>
2022-03-21 09:33:30 -0400

    edited join message

--- js/src/main/scala/poolmate/Message.scala
+++ js/src/main/scala/poolmate/Message.scala
@@ -1,5 +1,5 @@
 package poolmate
 
 object Message:
-  val joinMessage = "Record your pin to enter your account."
+  val joinMessage = "Record your pin. It allows you to enter your account."
   val pinMessage = "Keep your pin of 6 alphanumeric characters in a secure place."
