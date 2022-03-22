dbb5e3d171b270c3d934a30f4098f21cb7f88fa5
objektwerks <objektwerks@runbox.com>
2022-03-22 05:40:12 -0400

    reformatted root view

--- js/src/main/scala/poolmate/RootView.scala
+++ js/src/main/scala/poolmate/RootView.scala
@@ -7,32 +7,32 @@ import org.scalajs.dom.console.log
 import Components.*
 
 object RootView extends View:
-    def apply(pinVar: Var[String], accountVar: Var[Account]): HtmlElement =
-      def handler(event: Either[Fault, Event]): Unit =
-        event match
-          case Right(event) =>
-            event match
-              case Joined(account) =>
-                clearErrors()
-                accountVar.set(account)
-                pinVar.set(account.pin)
-                route(EnterPage)
-              case _ => log(s"Index -> Join view handler failed: $event")
-          case Left(fault) => errorBus.emit(s"Join failed: ${fault.cause}")
-      div(
-        bar(
-          btn("Enter").amend {
-            onClick --> { _ =>
-              log("Index -> Enter menu item onClick")
+  def apply(pinVar: Var[String], accountVar: Var[Account]): HtmlElement =
+    def handler(event: Either[Fault, Event]): Unit =
+      event match
+        case Right(event) =>
+          event match
+            case Joined(account) =>
+              clearErrors()
+              accountVar.set(account)
+              pinVar.set(account.pin)
               route(EnterPage)
-            }
-          },
-          rbtn("Join").amend {
-            onClick --> { _ =>
-              log(s"Index -> Join menu item onClick")
-              val command = Join()
-              call(command, handler)
-            }
-          }          
-        )
+            case _ => log(s"Index -> Join view handler failed: $event")
+        case Left(fault) => errorBus.emit(s"Join failed: ${fault.cause}")
+    div(
+      bar(
+        btn("Enter").amend {
+          onClick --> { _ =>
+            log("Index -> Enter menu item onClick")
+            route(EnterPage)
+          }
+        },
+        rbtn("Join").amend {
+          onClick --> { _ =>
+            log(s"Index -> Join menu item onClick")
+            val command = Join()
+            call(command, handler)
+          }
+        }          
       )
+    )
