8e1eb8ec77c6fe324942de3c122ed6cf08ae41c1
objektwerks <objektwerks@runbox.com>
2022-03-25 08:36:57 -0400

    added client to jvm

---
+++ jvm/src/main/scala/poolmate/Client.scala
@@ -0,0 +1,46 @@
+package poolmate
+
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
+
+import Serializers.given
+
+import upickle.default.*
+
+object Client extends LazyLogging:
+  def main(args: Array[String]): Unit =
+    val conf = ConfigFactory.load("server.conf")
+    val host = conf.getString("host")
+    val port = conf.getString("host")
+    val url = s"http://$host:$port/command"
+    
+    logger.info(s"*** Server url: $url")
+
+    val join = Join()
+    logger.info(s"*** join: $join")
+
+    val joinJson = write[Join](join)
+    logger.info(s"*** Join json: $joinJson")
+
+    val joinResponse = requests.post(url, data = joinJson)
+    logger.info(s"*** Join response: $joinResponse")
+    
+    val joined = read[Joined](joinResponse.text())
+    logger.info(s"*** Joined: $joined")
+
+    val account = joined.account
+    logger.info(s"*** Account: $account")
+
+    val enter = Enter(account.pin)
+    logger.info(s"*** Enter: $enter")
+
+    val enterJson = write[Enter](enter)
+    logger.info(s"*** Enter json: $enterJson")
+
+    val enterResponse = requests.post(url, data = enterJson)
+    logger.info(s"*** Enter response: $enterResponse")
+
+    val entered = read[Entered](enterResponse.text())
+    logger.info(s"*** Entered: $entered")
+
+    require(account == entered.account, "Account not equal to LoggedIn account.")
