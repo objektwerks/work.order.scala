7baa7f69324d00d23d6741cbbe8d59d06687eca5
objektwerks <objektwerks@runbox.com>
2022-03-25 09:43:49 -0400

    moved embedded server

--- jvm/src/test/scala/poolmate/RequesterTest.scala
+++ jvm/src/test/scala/poolmate/RequesterTest.scala
@@ -1,59 +1,16 @@
 package poolmate
 
-import cask.main.Main
-
-import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-import scala.concurrent.duration._
 import scala.sys.process.Process
-
-import io.undertow.Undertow
-import io.undertow.server.handlers.BlockingHandler
 
 import Serializers.given
 
 import upickle.default.*
-
-final class EmbeddedServer(conf: Config) extends Main with LazyLogging:
-  val store = Store(conf, Store.cache(minSize = 4, maxSize = 10, expireAfter = 24.hour))
-  val service = Service(store)
-  val authorizer = Authorizer(service)
-  val validator = Validator()
-  val dispatcher = Dispatcher(authorizer, validator, service)
-  val router = Router(dispatcher, store)
-
-  override val allRoutes = Seq(router)
-  
-  override def host: String = conf.getString("host")
-
-  override def port: Int = conf.getInt("port")
-
-  override def defaultHandler: BlockingHandler =
-    new BlockingHandler( CorsHandler(dispatchTrie,
-                                     mainDecorators,
-                                     debugMode = false,
-                                     handleNotFound,
-                                     handleMethodNotAllowed,
-                                     handleEndpointError) )
-
-  Main.silenceJboss()    
-  val server = Undertow.builder
-    .addHttpListener(port, host)
-    .setHandler(defaultHandler)
-    .build
-
-  def start(): Unit =
-    server.start()
-    logger.info(s"*** EmbeddedServer started at http://$host:$port/")
-
-  def stop(): Unit =
-    server.stop()
-    logger.info("*** EmbeddedServer stopped.")
 
 class RequesterTest extends AnyFunSuite with Matchers with LazyLogging:
   Process("psql -d poolmate -f ddl.sql").run().exitValue()
