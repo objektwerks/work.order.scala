e16db2ac5e1706161dee07b4ffa944bbd072fe93
objektwerks <objektwerks@runbox.com>
2022-05-13 05:53:14 -0400

    edited validators

--- shared/src/main/scala/poolmate/Validators.scala
+++ shared/src/main/scala/poolmate/Validators.scala
@@ -3,6 +3,7 @@ package poolmate
 object Validators:
   extension (value: String)
     def isLicense: Boolean = if value.nonEmpty then value.length == 36 else false
+    def isEmailAddress: Boolean = value.nonEmpty && value.length >= 3 && value.contains("@")
     def isPin: Boolean = value.length == 7
     def isName: Boolean = value.length >= 2 && value.length <= 24
 
