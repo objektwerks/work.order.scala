9a69c2e8b8df393b7cce518e64916aca6fbbfa91
objektwerks <objektwerks@runbox.com>
2022-05-13 05:52:07 -0400

    added join view

---
+++ js/src/main/scala/poolmate/JoinView.scala
@@ -0,0 +1,54 @@
+package poolmate
+
+import com.raquo.laminar.api.L.*
+
+import org.scalajs.dom.console.log
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+import Components.*
+import Error.*
+import Message.*
+import Validators.*
+
+object JoinView extends View:
+  def apply(emailAddressVar: Var[String], pinVar: Var[String], accountVar: Var[Account]): HtmlElement =
+    val emailAddressErrorBus = new EventBus[String]
+
+    def handler(either: Either[Fault, Event]): Unit =
+      either match
+        case Left(fault) => errorBus.emit(s"Register failed: ${fault.cause}")
+        case Right(event) =>
+          event match
+            case Joined(account) =>
+              clearErrors()
+              accountVar.set(account)
+              pinVar.set(account.pin)
+              route(EnterPage)
+            case _ => log(s"Register -> handler failed: $event")
+      
+    div(
+      hdr("Register"),
+      info(registerMessage),
+      err(errorBus),
+      lbl("Email Address"),
+      email.amend {
+        value <-- emailAddressVar
+        onInput.mapToValue.filter(_.nonEmpty).setAsValue --> emailAddressVar
+        onKeyUp.mapToValue --> { emailAddress =>
+          if emailAddress.isEmailAddress then clear(emailAddressErrorBus) else emit(emailAddressErrorBus, emailAddressError)
+        }
+      },
+      err(emailAddressErrorBus),
+      cbar(
+        btn("Register").amend {
+          disabled <-- emailAddressVar.signal.map(email => !email.isEmailAddress)
+          onClick --> { _ =>
+            log(s"Register button onClick -> email address: ${emailAddressVar.now()}")
+            val command = Register(emailAddressVar.now())
+            call(command, handler)
+          }
+        },
+      )
+    )
--- js/src/main/scala/poolmate/Message.scala
+++ js/src/main/scala/poolmate/Message.scala
@@ -1,4 +1,5 @@
 package poolmate
 
 object Message:
+  val registerMessage = "Upon success, you'll transition to the Enter page. Record your pin! You'll also receive an email."
   val pinMessage = "Record your pin. It allows you to enter your account."
