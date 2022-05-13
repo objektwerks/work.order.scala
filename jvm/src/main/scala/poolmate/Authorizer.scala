cbd504c6539a7ebad0b0708cbff5e099eadfcc19
objektwerks <objektwerks@runbox.com>
2022-05-13 05:11:17 -0400

    edited authorizer

--- jvm/src/main/scala/poolmate/Authorizer.scala
+++ jvm/src/main/scala/poolmate/Authorizer.scala
@@ -6,4 +6,4 @@ final class Authorizer(service: Service):
   def authorize(command: Command): Event =
     command match
       case license: License => service.authorize(license.license)
-      case Join() | Enter(_) => Authorized("")
+      case Join(_) | Enter(_, _) => Authorized("")
