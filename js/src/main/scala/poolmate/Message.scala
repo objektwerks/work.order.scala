ef702b8f95456feb8fb3a5beda58d071cd28c734
objektwerks <objektwerks@runbox.com>
2022-05-13 07:48:36 -0400

    edited message

--- js/src/main/scala/poolmate/Message.scala
+++ js/src/main/scala/poolmate/Message.scala
@@ -2,4 +2,4 @@ package poolmate
 
 object Message:
   val joinMessage = "Upon success, you'll transition to the Enter page. Record your pin! You'll also receive an email."
-  val pinMessage = "Record your pin. It allows you to enter your account."
+  val pinMessage = "Record your pin! It and your email address allow you to enter your account."
