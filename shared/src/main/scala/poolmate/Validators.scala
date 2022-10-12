907471e4434439acf07e59cbaf4c051bd43a7344
objektwerks <objektwerks@runbox.com>
2022-10-12 09:04:58 -0400

    enhanced validators

--- shared/src/main/scala/poolmate/Validators.scala
+++ shared/src/main/scala/poolmate/Validators.scala
@@ -1,28 +1,29 @@
 package poolmate
 
 object Validators:
-  val licenseInvalidMessage = "A valid license is 36 characters."
-  val roleInvalidMessage = "A valid role must be selected."
-  val nameInvalidMessage = "For name, enter at least 2 characters."
-  val emailAddressInvalidMessage = "For email address, enter at least 3 characters to inlcude @."
-  val streetAddressInvalidMessage = "For street address, enter at least 6 characters."
-  val pinInvalidMessage = "For pin, enter exactly 7 numbers, characters and/or symbols."
-  val datetimeInvalidMessage = "For datetime, must use 24-character ISO standard: YYYY-MM-DDTHH:mm:ss.sssZ"
+  val licenseInvalidMessage = "A license must be 36 characters."
+  val roleInvalidMessage = "A role must be selected."
+  val nameInvalidMessage = "A name must be at least 2 characters."
+  val emailAddressInvalidMessage = "An email address must be at least 3 characters and inlcude an @ symbol."
+  val streetAddressInvalidMessage = "A street address must at least 6 characters."
+  val pinInvalidMessage = "A pin must be exactly 7 numbers, characters and/or symbols."
+  val datetimeInvalidMessage = "A datetime must be 24-characters, using ISO standard: YYYY-MM-DDTHH:mm:ss.sssZ"
   val idInvalidMessage = "An id must be greater than 0."
   val numberInvalidMessage = "A number must be greater than 0."
-  val definedInvalidMessage = "This field may be empty, but must be defined."
 
   extension (value: String)
-    def isLicense: Boolean = if value.nonEmpty then value.length == 36 else false
-    def isEmailAddress: Boolean = value.nonEmpty && value.length >= 3 && value.contains("@")
+    def isLicense: Boolean = value.length == 36
+    def isEmailAddress: Boolean = value.length >= 3 && value.length <= 128 && value.contains("@")
+    def isStreetAddress: Boolean = value.length >= 6 && value.length <= 128
     def isPin: Boolean = value.length == 7
-    def isName: Boolean = value.length >= 2 && value.length <= 24
+    def isName: Boolean = value.length >= 2 && value.length <= 64
+    def isRole: Boolean = value == homeowner || value == serviceProvider
+    def isImageUrl: Boolean = value.startsWith("/images/")
 
   extension (value: Int)
-    def isGreaterThan1899 = value > 1899
-    def isGreaterThan999 = value > 999
     def isZero: Boolean = value == 0
     def isGreaterThanZero: Boolean = value > 0
+    def isGreaterThanOrEqualZero = value >= 0
 
   extension (user: User)
     def isValid: List[String] = List()
