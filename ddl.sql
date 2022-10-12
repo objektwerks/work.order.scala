b4a38ecaaadb5c863e1a77d379d2c4d5fe4c70a3
objektwerks <objektwerks@runbox.com>
2022-10-12 07:44:16 -0400

    edited ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,147 +1,35 @@
-DROP SCHEMA PUBLIC CASCADE;
-CREATE SCHEMA PUBLIC;
+drop database if exists `work_order_db`;
 
-CREATE TABLE account (
-  id BIGSERIAL PRIMARY KEY,
-  license VARCHAR(36) UNIQUE NOT NULL,
-  email_address VARCHAR NOT NULL,
-  pin VARCHAR(7) NOT NULL,
-  activated INT NOT NULL,
-  deactivated INT NOT NULL
+create database `work_order_db`;
+
+use `work_order_db`;
+
+create table `user` (
+  `id` int not null auto_increment,
+  `role` varchar(24) not null,
+  `name` varchar(64) not null,
+  `emailAddress` varchar(128) not null unique,
+  `streetAddress` varchar(128) not null unique,
+  `registered` varchar(24) not null,
+  `pin` varchar(7) not null unique,
+  `license` char(36) not null default (uuid()),
+  primary key (`id`)
 );
+create index user_email_address_idx ON `user`(`emailAddress`);
+create index user_pin_idx ON `user`(`pin`);
 
-CREATE TABLE pool (
-  id BIGSERIAL PRIMARY KEY,
-  license VARCHAR(36) REFERENCES account(license),
-  name VARCHAR(24) NOT NULL,
-  built INT NOT NULL,
-  volume INT NOT NULL,
-  cost INT NOT NULL
-);
-
-CREATE TABLE measurement (
-  id BIGSERIAL PRIMARY KEY,
-  pool_id BIGINT REFERENCES pool(id),
-  measured INT NOT NULL,
-  temp INT NOT NULL,
-  total_hardness INT NOT NULL,
-  total_chlorine INT NOT NULL,
-  total_bromine INT NOT NULL,
-  free_chlorine INT NOT NULL,
-  ph NUMERIC(2, 1) NOT NULL,
-  total_alkalinity INT NOT NULL,
-  cyanuric_acid INT NOT NULL
-);
-
-CREATE TABLE cleaning (
-  id BIGSERIAL PRIMARY KEY,
-  pool_id BIGINT REFERENCES pool(id),
-  cleaned INT NOT NULL,
-  brush BOOL NOT NULL,
-  net BOOL NOT NULL,
-  vacuum BOOL NOT NULL,
-  skimmer_basket BOOL NOT NULL,
-  pump_basket BOOL NOT NULL,
-  pump_filter BOOL NOT NULL,
-  deck BOOL NOT NULL
-);
-
-CREATE TABLE chemical (
-  id BIGSERIAL PRIMARY KEY,
-  pool_id BIGINT REFERENCES pool(id),
-  added INT NOT NULL,
-  chemical VARCHAR NOT NULL,
-  amount INT,
-  unit VARCHAR NOT NULL
-);
-
-CREATE TABLE supply (
-  id BIGSERIAL PRIMARY KEY,
-  pool_id BIGINT REFERENCES pool(id),
-  purchased INT NOT NULL,
-  item VARCHAR NOT NULL,
-  amount INT NOT NULL,
-  unit VARCHAR NOT NULL,
-  cost INT NOT NULL
-);
-
-CREATE TABLE repair (
-  id BIGSERIAL PRIMARY KEY,
-  pool_id BIGINT REFERENCES pool(id),
-  repaired INT NOT NULL,
-  repair VARCHAR NOT NULL,
-  cost INT NOT NULL
-);
-
-CREATE TABLE pump (
-  id BIGSERIAL PRIMARY KEY,
-  pool_id BIGINT REFERENCES pool(id),
-  installed INT NOT NULL,
-  model VARCHAR NOT NULL,
-  cost INT NOT NULL
-);
-
-CREATE TABLE timer (
-  id BIGSERIAL PRIMARY KEY,
-  pool_id BIGINT REFERENCES pool(id),
-  installed INT NOT NULL,
-  model VARCHAR NOT NULL,
-  cost INT NOT NULL
-);
-
-CREATE TABLE timer_setting (
-  id BIGSERIAL PRIMARY KEY,
-  timer_id BIGINT REFERENCES timer(id),
-  date_set INT NOT NULL,
-  time_on SMALLINT NOT NULL,
-  time_off SMALLINT NOT NULL
-);
-
-CREATE TABLE heater (
-  id BIGSERIAL PRIMARY KEY,
-  pool_id BIGINT REFERENCES pool(id),
-  installed INT NOT NULL,
-  model VARCHAR NOT NULL,
-  cost INT NOT NULL
-);
-
-CREATE TABLE heater_setting (
-  id BIGSERIAL PRIMARY KEY,
-  heater_id BIGINT REFERENCES heater(id),
-  date_set INT NOT NULL,
-  temp INT NOT NULL
-);
-
-CREATE TABLE surface (
-  id BIGSERIAL PRIMARY KEY,
-  pool_id BIGINT REFERENCES pool(id),
-  installed INT NOT NULL,
-  kind VARCHAR NOT NULL,
-  cost INT NOT NULL
-);
-
-CREATE TABLE deck (
-  id BIGSERIAL PRIMARY KEY,
-  pool_id BIGINT REFERENCES pool(id),
-  installed INT NOT NULL,
-  kind VARCHAR NOT NULL,
-  cost INT NOT NULL
-);
-
-CREATE TABLE email (
-  id VARCHAR PRIMARY KEY,
-  license VARCHAR(36) REFERENCES account(license),
-  address VARCHAR NOT NULL,
-  date_sent INT NOT NULL,
-  time_sent INT NOT NULL,
-  processed BOOL NOT NULL,
-  valid BOOL NOT NULL
-);
-
-CREATE TABLE fault (
-  date_of INT NOT NULL,
-  time_of INT NOT NULL,
-  nano_of Int NOT NULL,
-  cause VARCHAR NOT NULL,
-  PRIMARY KEY (date_of, time_of, nano_of)
+create table `work_order` (
+  `number` int not null auto_increment,
+  `homeownerId` int not null,
+  `serviceProviderId` int not null,
+  `title` varchar(64) not null,
+  `issue` varchar(255) not null,
+  `streetAddress` varchar(128) not null,
+  `imageUrl` varchar(255) not null default "",
+  `resolution` varchar(255) not null default "",
+  `opened` varchar(24) not null,
+  `closed` varchar(24) not null default "",
+  primary key (`number`),
+  constraint homeowner_id_fk foreign key (`homeownerId`) REFERENCES `user`(`id`),
+  constraint service_provider_id_fk foreign key (`serviceProviderId`) REFERENCES `user`(`id`)
 );
