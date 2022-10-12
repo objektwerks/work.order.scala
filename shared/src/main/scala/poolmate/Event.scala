72573c606e06076cffdb99d41a42915810e30d3d
objektwerks <objektwerks@runbox.com>
2022-10-12 10:50:07 -0400

    added work order saved event

--- shared/src/main/scala/poolmate/Event.scala
+++ shared/src/main/scala/poolmate/Event.scala
@@ -13,32 +13,22 @@ final case class LoggedIn(user: User,
                           workOrders: Array[WorkOrder], 
                           success: Boolean = true, 
                           error: String = "") extends Event:
-
   def success(user: User, serviceProviders: Array[User], workOrders: Array[WorkOrder]): LoggedIn = LoggedIn(user, serviceProviders, workOrders)
   def fail(error: String): LoggedIn = LoggedIn(User.empty, Array.empty[User], Array.empty[WorkOrder], false, error)
 
 final case class UserSaved(id: Int, 
                            success: Boolean = true, 
                            error: String = "") extends Event:
-
   def success(id: Int): UserSaved = UserSaved(id)
   def fail(id: Int, error: String): UserSaved = UserSaved(id, false, error)
 
+final case class WorkOrderSaved(number: Int,
+                                success: Boolean = true,
+                                error: String = "") extends Event:
+  def success(number: Int): WorkOrderSaved = WorkOrderSaved(number)
+  def fail(number: Int, error: String): WorkOrderSaved = WorkOrderSaved(number, false, error)
+
 /*
-export class WorkOrderSaved {
-  constructor(public number: number,
-              public success: boolean = true,
-              public error: string = '') {}
-
-  static success(number: number): WorkOrderSaved {
-    return new WorkOrderSaved(number)
-  }
-  
-  static fail(number: number, error: string): WorkOrderSaved {
-    return new WorkOrderSaved(number, false, error)
-  }
-}
-
 export class WorkOrdersListed {
   constructor(public userId: number, 
               public workOrders: WorkOrder[], 
