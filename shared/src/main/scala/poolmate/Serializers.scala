e23514d41b27a869872677dc3c2851b63e59def4
objektwerks <objektwerks@runbox.com>
2022-10-12 10:14:37 -0400

    enhanced serializers

--- shared/src/main/scala/poolmate/Serializers.scala
+++ shared/src/main/scala/poolmate/Serializers.scala
@@ -5,3 +5,4 @@ object Serializers:
 
   given userRW: ReadWriter[User] = macroRW
   given workOrderRW: ReadWriter[WorkOrder] = macroRW
+  given entityRW: ReadWriter[Entity] = ReadWriter.merge( userRW, workOrderRW )
