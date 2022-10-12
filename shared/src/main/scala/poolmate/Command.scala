f90b5a85fe1ddeb708a0af26721467a7d91f217b
objektwerks <objektwerks@runbox.com>
2022-10-12 10:13:00 -0400

    implemented commands

--- shared/src/main/scala/poolmate/Command.scala
+++ shared/src/main/scala/poolmate/Command.scala
@@ -1,3 +1,16 @@
 package poolmate
 
 sealed trait Command
+
+final case class Register(role: String,
+                          name: String,
+                          emailAddress: String,
+                          streetAddress: String)
+
+final case class Login(emailAddress: String, pin: String) extends Command
+
+final case class SaveUser(user: User) extends Command
+
+final case class SaveWorkOrder(workOrder: WorkOrder, license: String) extends Command
+
+final case class ListWorkOrders(userId: Int, license: String) extends Command
