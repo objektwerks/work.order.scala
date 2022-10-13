04fa5fc2c893f92e00f52ec13e075f400d293b9e
objektwerks <objektwerks@runbox.com>
2022-10-13 07:39:20 -0400

    refactored dispatcher test

--- jvm/src/test/scala/poolmate/DispatcherTest.scala
+++ jvm/src/test/scala/poolmate/DispatcherTest.scala
@@ -11,18 +11,16 @@ import org.scalatest.matchers.should.Matchers
 import scala.concurrent.duration._
 import scala.sys.process.Process
 
-import Validators.*
+import Validator.*
 
 class DispatcherTest extends AnyFunSuite with Matchers with LazyLogging:
   Process("psql -d poolmate -f ddl.sql").run().exitValue()
 
   val conf = ConfigFactory.load("test.server.conf")
   val store = Store(conf, Store.cache(minSize = 4, maxSize = 10, expireAfter = 24.hour))
-  val emailSender = EmailSender(conf, store)
+  val emailer = Emailer(conf, store)
   val service = Service(store)
-  val authorizer = Authorizer(service)
-  val validator = Validator()
-  val dispatcher = Dispatcher(authorizer, validator, service, emailSender)
+  val dispatcher = Dispatcher(emailer, service)
 
   test("dispatcher") {
     testDispatcher(dispatcher, store)
