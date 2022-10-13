cf5d82decb2f2363ab8e5b6cc1d41638040f02bf
objektwerks <objektwerks@runbox.com>
2022-10-13 10:15:03 -0400

    enhanced service

--- jvm/src/main/scala/poolmate/Service.scala
+++ jvm/src/main/scala/poolmate/Service.scala
@@ -2,4 +2,11 @@ package poolmate
 
 final class Service(store: Store):
   def register(register: Register): Registered = ???
+
   def login(login: Login): LoggedIn = ???
+
+  def saveUser(saveUser: SaveUser): UserSaved = ???
+
+  def saveWorkOrder(saveWorkOrder: SaveWorkOrder): WorkOrderSaved = ???
+  
+  def listWorkOrders(listWorkOrders: ListWorkOrders): WorkOrdersListed = ???
