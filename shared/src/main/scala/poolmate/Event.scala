0d0fc44de1c68eb4ad60108e3efdb5eb92f8e354
objektwerks <objektwerks@runbox.com>
2022-10-13 10:19:32 -0400

    refactored event

--- shared/src/main/scala/poolmate/Event.scala
+++ shared/src/main/scala/poolmate/Event.scala
@@ -4,7 +4,9 @@ sealed trait Event
 
 final case class Registered(pin: String,
                             success: Boolean = true,
-                            error: String = "") extends Event:
+                            error: String = "") extends Event
+
+object Registered:
   def success(pin: String): Registered = Registered(pin)
   def fail(error: String): Registered = Registered("", false, error)
 
@@ -12,25 +14,33 @@ final case class LoggedIn(user: User,
                           serviceProviders: Array[User], 
                           workOrders: Array[WorkOrder], 
                           success: Boolean = true, 
-                          error: String = "") extends Event:
+                          error: String = "") extends Event
+
+object LoggedIn:
   def success(user: User, serviceProviders: Array[User], workOrders: Array[WorkOrder]): LoggedIn = LoggedIn(user, serviceProviders, workOrders)
   def fail(error: String): LoggedIn = LoggedIn(User.empty, Array.empty[User], Array.empty[WorkOrder], false, error)
 
 final case class UserSaved(id: Int, 
                            success: Boolean = true, 
-                           error: String = "") extends Event:
+                           error: String = "") extends Event
+
+object UserSaved:
   def success(id: Int): UserSaved = UserSaved(id)
   def fail(id: Int, error: String): UserSaved = UserSaved(id, false, error)
 
 final case class WorkOrderSaved(number: Int,
                                 success: Boolean = true,
-                                error: String = "") extends Event:
+                                error: String = "") extends Event
+
+object WorkOrderSaved:
   def success(number: Int): WorkOrderSaved = WorkOrderSaved(number)
   def fail(number: Int, error: String): WorkOrderSaved = WorkOrderSaved(number, false, error)
 
 final case class WorkOrdersListed(userId: Int, 
                                   workOrders: Array[WorkOrder], 
                                   success: Boolean = true, 
-                                  error: String = "") extends Event:
+                                  error: String = "") extends Event
+
+object WorkOrdersListed:
   def success(userId: Int, workOrders: Array[WorkOrder]): WorkOrdersListed = WorkOrdersListed(userId, workOrders)
   def fail(userId: Int, error: String): WorkOrdersListed = WorkOrdersListed(userId, Array.empty[WorkOrder], false, error)
