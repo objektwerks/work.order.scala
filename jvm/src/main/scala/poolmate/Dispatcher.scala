4b7cd75bea76b804af6f2fc5d6030e5056ad6359
objektwerks <objektwerks@runbox.com>
2022-10-13 10:26:36 -0400

    refactored dispatcher and service

--- jvm/src/main/scala/poolmate/Dispatcher.scala
+++ jvm/src/main/scala/poolmate/Dispatcher.scala
@@ -4,8 +4,17 @@ final class Dispatcher(emailer: Emailer,
                        service: Service):
   def dispatch(command: Command): Event =
     command match
-      case register: Register => service.register(register)
-      case login: Login => service.login(login)
+      case register: Register =>
+        service.register(register)
+      
+      case login: Login =>
+        service.login(login)
+      
       case saveUser: SaveUser =>
+        service.saveUser(saveUser)
+      
       case saveWorkOrder: SaveWorkOrder =>
+        service.saveWorkOrder(saveWorkOrder)
+      
       case listWorkOrders: ListWorkOrders =>
+        service.listWorkOrders(listWorkOrders)
--- jvm/src/main/scala/poolmate/Service.scala
+++ jvm/src/main/scala/poolmate/Service.scala
@@ -1,12 +1,12 @@
 package poolmate
 
 final class Service(store: Store):
-  def register(register: Register): Registered = ???
+  def register(register: Register): Registered = Registered.success(User.newPin)
 
-  def login(login: Login): LoggedIn = ???
+  def login(login: Login): LoggedIn = LoggedIn.success(User.empty, Array.empty[User], Array.empty[WorkOrder])
 
-  def saveUser(saveUser: SaveUser): UserSaved = ???
+  def saveUser(saveUser: SaveUser): UserSaved = UserSaved.success(saveUser.user.id)
 
-  def saveWorkOrder(saveWorkOrder: SaveWorkOrder): WorkOrderSaved = ???
-  
-  def listWorkOrders(listWorkOrders: ListWorkOrders): WorkOrdersListed = ???
+  def saveWorkOrder(saveWorkOrder: SaveWorkOrder): WorkOrderSaved = WorkOrderSaved.success(saveWorkOrder.workOrder.number)
+
+  def listWorkOrders(listWorkOrders: ListWorkOrders): WorkOrdersListed = WorkOrdersListed.success(listWorkOrders.userId, Array.empty[WorkOrder])
