283e545b7718175b3a54ea75694a0164bc5dbbbf
objektwerks <objektwerks@runbox.com>
2022-10-16 16:46:00 -0400

    reimplemented handler test

--- jvm/src/test/scala/poolmate/HandlerTest.scala
+++ jvm/src/test/scala/poolmate/HandlerTest.scala
@@ -3,6 +3,8 @@ package poolmate
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
+import java.time.Instant
+
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
@@ -26,41 +28,51 @@ class HandlerTest extends AnyFunSuite with Matchers with LazyLogging:
     val homeownerEmail = conf.getString("email.homeownerEmail")
 
     // register
-    val serviceProviderRegistered = handler.register(Register(Roles.serviceProvider, "lawncare service", serviceProviderEmail, "123 green rd"))
-    val homeownerRegistered = handler.register(Register(Roles.homeowner, "fred flintstone", homeownerEmail, "345 stone st"))
-    assert(serviceProviderRegistered.success, "*** register service provider failed: ${toJson(serviceProviderRegistered)}")
-    assert(homeownerRegistered.success, "*** register homeowner failed: ${toJson(homeownerRegistered)}")
+    val serviceProviderRegister = Register(Roles.serviceProvider, "lawncare service", serviceProviderEmail, "123 green rd")
+    val serviceProviderPin = handler.handle(serviceProviderRegister) match
+      case registered: Registered => registered.success shouldBe true; registered.pin
+      case _ => fail()
+
+    val homeownerRegister = Register(Roles.homeowner, "fred flintstone", homeownerEmail, "345 stone st")
+    val homeownerPin = handler.handle(homeownerRegister) match
+      case registered: Registered => registered.success shouldBe true; registered.pin
+      case _ => fail()
 
     // login
-    val serviceProviderLoggedIn = handler.login(Login(serviceProviderEmail, serviceProviderRegistered.pin))
-    val homeownerLoggedIn =  handler.login(new Login(homeownerEmail, homeownerRegistered.pin))
-    assert(serviceProviderLoggedIn.success, "*** login service provider failed: ${toJson(serviceProviderLoggedIn)}")
-    assert(serviceProviderLoggedIn.user.pin === serviceProviderRegistered.pin, "*** logged in service provider pin is invalid: ${serviceProviderRegistered.pin}")
-    assert(homeownerLoggedIn.success, "*** login homeowner failed: ${toJson(homeownerLoggedIn)}")
-    assert(homeownerLoggedIn.user.pin === homeownerRegistered.pin, "*** logged in homeowner pin is invalid: ${homeownerRegistered.pin}")
+    val serviceProviderLogin = Login(serviceProviderEmail, serviceProviderPin)
+    val serviceProviderLoggedIn = handler.handle(serviceProviderLogin) match
+      case loggedIn: LoggedIn => loggedIn.success shouldBe true; loggedIn
+      case _ => fail()
+    
+    val homeownerLogin = Login(homeownerEmail, homeownerPin)
+    val homeownerLoggedIn = handler.handle(homeownerLogin) match
+      case loggedIn: LoggedIn => loggedIn.success shouldBe true; loggedIn
+      case _ => fail()
 
     // work order add
-    val workOrder = new WorkOrder(0, homeownerLoggedIn.user.id, serviceProviderLoggedIn.user.id, "sprinkler", "345 stone st", "broken", "", "", new Date().toISOString(), "")
-    val workOrderAdded =  handler.addWorkOrder(new SaveWorkOrder(workOrder, homeownerLoggedIn.user.license))
-    workOrder.number = workOrderAdded.number
-    assert(workOrderAdded.success, "*** add work order failed: ${toJson(workOrderAdded)}")
-    assert(workOrderAdded.number > 0, "*** work order number invalid: ${workOrderAdded.number}")
+    var workOrder = WorkOrder(0, homeownerLoggedIn.user.id, serviceProviderLoggedIn.user.id, "sprinkler", "345 stone st", "broken", "", "", Instant.now().toString(), "")
+    workOrder = handler.handle(AddWorkOrder(workOrder, homeownerLoggedIn.user.license)) match
+      case workOrderAdded: WorkOrderAdded => workOrderAdded.success shouldBe true; workOrder.copy(number = workOrderAdded.number)
+      case _ => fail()
 
     // work order save
-    workOrder.resolution = "fixed"
-    workOrder.closed = new Date().toISOString()
-    val workOrderSaved =  handler.saveWorkOrder(new SaveWorkOrder(workOrder, homeownerLoggedIn.user.license))
-    assert(workOrderSaved.success, "*** save work order failed: ${toJson(workOrderSaved)}")
-
+    workOrder = workOrder.copy(resolution = "fixed")
+    workOrder.copy(closed = Instant.now().toString())
+    handler.handle(SaveWorkOrder(workOrder, homeownerLoggedIn.user.license)) match
+      case workOrderSaved: WorkOrderSaved => workOrderSaved.success shouldBe true; workOrderSaved
+      case _ => fail()
+    
     // user save
-    val serviceProviderUserSaved =  handler.saveUser(new SaveUser(serviceProviderLoggedIn.user))
-    val homeownerUserSaved =  handler.saveUser(new SaveUser(homeownerLoggedIn.user))
-    assert(serviceProviderUserSaved.success, "*** save service provider user failed: ${serviceProviderUserSaved}")
-    assert(homeownerUserSaved.success, "*** save homeowner user failed: ${toJson(homeownerUserSaved)}")
+    handler.handle(SaveUser(serviceProviderLoggedIn.user)) match
+      case userSaved: UserSaved => userSaved.success shouldBe true
+      case _ => fail()
+
+    handler.handle(SaveUser(homeownerLoggedIn.user)) match
+      case userSaved: UserSaved => userSaved.success shouldBe true
+      case _ => fail()
 
     // work orders list
-    val workOrdersListed =  handler.listWorkOrders(new ListWorkOrders(homeownerLoggedIn.user.id, homeownerLoggedIn.user.license))
-    assert(workOrdersListed.success, "*** list work orders failed: ${toJson(workOrdersListed)}")
-    assert(workOrdersListed.workOrders.length === 1, "*** list work orders size !== 1")
-
+    handler.handle(ListWorkOrders(homeownerLoggedIn.user.id, homeownerLoggedIn.user.license)) match
+      case workOrdersListed: WorkOrdersListed => workOrdersListed.success shouldBe true; workOrdersListed.workOrders.size shouldBe 1
+      case _ => fail()
   }
