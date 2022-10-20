d1d163928bc71766a2852d7f1c9d42ac16eed145
objektwerks <objektwerks@runbox.com>
2022-10-20 16:04:38 -0400

    removed proxy

--- js/src/main/scala/workorder/Client.scala
+++ js/src/main/scala/workorder/Client.scala
@@ -13,7 +13,7 @@ import scala.scalajs.js.annotation.JSExportTopLevel
 class Client(rootUrl: String) extends js.Object:
   Urls.set(rootUrl)
 
-  Proxy.now.foreach( now => log(s"[now] $now") )
+  // User fetcher instead: Proxy.now.foreach( now => log(s"[now] $now") )
 
   render(
     container = document.getElementById("content"),
