e6a82ac4f45020a3e9a5c8f16139a019a4f29e92
objektwerks <objektwerks@runbox.com>
2022-10-20 13:31:07 -0400

    edited work orders view

--- js/src/main/scala/workorder/WorkOrdersView.scala
+++ js/src/main/scala/workorder/WorkOrdersView.scala
@@ -10,14 +10,6 @@ import Validator.*
 object WorkOrdersView extends View:
   def apply(workOrdersVar: Var[Array[WorkOrder]]): HtmlElement =
     div(
-      bar(
-        btn("App").amend {
-          onClick --> { _ =>
-            log("Account -> App menu item onClick")
-            route(RootPage)
-          }
-        }      
-      ),
       div(
         hdr("Work Orders"),
         // List of opened and closed work orders.
