ec805ec2944eed7031540b2ce2074aeaccdfb213
objektwerks <objektwerks@runbox.com>
2022-10-22 16:00:46 -0400

    edited view

--- js/src/main/scala/workorder/View.scala
+++ js/src/main/scala/workorder/View.scala
@@ -5,7 +5,7 @@ import com.raquo.laminar.api.L.*
 trait View:
   protected[this] val errorBus = new EventBus[String]
 
-  def call(command: Command, handler: (Either[Fault, Event]) => Unit): Unit = Fetcher.call(command, handler)
+  def call(command: Command, handler: Either[Fault, Event] => Unit): Unit = Fetcher.call(command, handler)
   
   def route(page: Page): Unit = PageRouter.router.pushState(page)
 
