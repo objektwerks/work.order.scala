112eba34f77b78684676d15c9c8613fbe4760cca
objektwerks <objektwerks@runbox.com>
2022-10-22 13:13:03 -0400

    edited logging

--- js/src/main/scala/workorder/LoginRegisterMenu.scala
+++ js/src/main/scala/workorder/LoginRegisterMenu.scala
@@ -12,13 +12,13 @@ object LoginRegisterMenu extends View:
       bar(
         btn("Login").amend {
           onClick --> { _ =>
-            log("Root -> Login menu item onClick")
+            log("LoginRegister -> Login menu item onClick")
             route(LoginPage)
           }
         },
         rbtn("Register").amend {
           onClick --> { _ =>
-            log(s"Root -> Register menu item onClick")
+            log(s"LoginRegister -> Register menu item onClick")
             route(RegisterPage)
           }
         }          
