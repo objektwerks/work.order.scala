d63c37e8b2c65734f08dd71785f664283a624d54
objektwerks <objektwerks@runbox.com>
2022-10-23 08:53:37 -0400

    refactored components

--- js/src/main/scala/workorder/Components.scala
+++ js/src/main/scala/workorder/Components.scala
@@ -14,37 +14,25 @@ object Components:
     div(cls("w3-bar w3-margin-top w3-center"), elms)
 
   def btn(text: String): Button =
-    button(cls("w3-button w3-round-xxlarge w3-light-grey w3-text-indigo"), text)
+    button(cls("w3-button w3-round w3-indigo"), text)
 
   def rbtn(text: String): Button =
-    button(cls("w3-button w3-round-xxlarge w3-light-grey w3-text-indigo w3-right"), text)
-  
-  def dropdown(header: Button, buttons: Button*): Div =
-    div(cls("w3-dropdown-hover"), header, div(cls("w3-dropdown-content w3-bar-block w3-card-4"), buttons))
+    button(cls("w3-button w3-round w3-indigo w3-right"), text)
 
   def lbl(text: String): Label =
     label(cls("w3-left-align w3-text-indigo"), text)
-
-  def info(text: String): Div =
-    div(cls("w3-border-white w3-text-indigo"), text)
 
   def txt: Input =
     input(cls(inputCss), required(true))
 
   def rotxt: Input =
-    input(cls(inputCss), readOnly(true))
+    input(cls("w3-input w3-light-gray w3-text-indigo"), readOnly(true))
 
   def email: Input =
     input(cls(inputCss), typ("email"), minLength(3), required(true))
 
   def pin: Input =
     input(cls(inputCss), typ("text"), minLength(7), maxLength(7), required(true))
-
-  def date: Input =
-    input(cls(inputCss), tpe("date"), required(true))
- 
-  def time: Input =
-    input(cls(inputCss), tpe("time"), required(true))
 
   def int: Input =
     input(cls(inputCss), typ("number"), pattern("\\d*"), stepAttr("1"), required(true))
@@ -53,16 +41,7 @@ object Components:
     input(cls(inputCss), typ("number"), pattern("[0-9]+([.,][0-9]+)?"), stepAttr("0.01"), required(true))
 
   def hdr(text: String): HtmlElement =
-    h5(cls("w3-light-grey w3-text-indigo"), text)
+    h5(cls("w3-indigo"), text)
 
   def err(errBus: EventBus[String]): Div =
-    div(cls("w3-border-white w3-text-red"), child.text <-- errBus.events)
-
-  def msg(noteBus: EventBus[String]): Div =
-    div(cls("w3-border-white w3-text-indigo"), child.text <-- noteBus.events)
-
-  def list(liSignal: Signal[Seq[Li]]): Div =
-    div(cls("w3-container"), ul(cls("w3-ul w3-hoverable"), children <-- liSignal))
-
-  def item(strSignal: Signal[String]): Li =
-    li(cls("w3-text-indigo w3-display-container"), child.text <-- strSignal)
+    div(cls("w3-red"), child.text <-- errBus.events)
