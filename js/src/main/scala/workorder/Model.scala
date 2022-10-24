02b698664a1af9a01661a934ee69f808e488c3f6
objektwerks <objektwerks@runbox.com>
2022-10-24 16:36:43 -0400

    refactored open/closed work orders in model

--- js/src/main/scala/workorder/Model.scala
+++ js/src/main/scala/workorder/Model.scala
@@ -20,6 +20,6 @@ object Model:
 
   def serviceProviderName(id: Int): String = serviceProvidersVar.now().find(_.id == id).fold("")(_.name)
 
-  def openWorkOrders: List[WorkOrder] = workOrdersVar.now().filter(workOrder => workOrder.closed.isEmpty)
+  def openWorkOrders: Var[List[WorkOrder]] = Var(workOrdersVar.now().filter(workOrder => workOrder.closed.isEmpty))
 
-  def closedWorkOrders: List[WorkOrder] = workOrdersVar.now().filter(workOrder => workOrder.closed.nonEmpty)
+  def closedWorkOrders: Var[List[WorkOrder]] = Var(workOrdersVar.now().filter(workOrder => workOrder.closed.nonEmpty))
