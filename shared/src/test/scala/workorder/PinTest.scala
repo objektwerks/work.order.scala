b4774a68390f74214304bdc239dba6e929947efd
objektwerks <objektwerks@runbox.com>
2022-10-27 14:12:38 -0400

    refactored datatime; removed shared unit tests

--- shared/src/test/scala/workorder/PinTest.scala
+++
@@ -1,9 +0,0 @@
-package workorder
-
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers
-
-class PinTest extends AnyFunSuite with Matchers:
-  test("pin") {
-    User.newPin.length shouldBe 7
-  }
