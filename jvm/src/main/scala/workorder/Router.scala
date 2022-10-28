601c3c2666ccddc27fb926e858db89b83b77c962
objektwerks <objektwerks@runbox.com>
2022-10-28 17:26:51 -0400

    edited router

--- jvm/src/main/scala/workorder/Router.scala
+++ jvm/src/main/scala/workorder/Router.scala
@@ -10,6 +10,12 @@ import upickle.default.{read, write}
 
 import Serializer.given
 
+/* Note: This router can be readily set up as:
+   https://github.com/objektwerks/poolmate.x/blob/main/jvm/src/main/scala/poolmate/Router.scala
+   But this router is a replica of: https://github.com/objektwerks/work.order for various reasons,
+   to include the optional support of images. Image file support is implemented in js/Fetcher. But
+   it is not implemented in this router. Scala has no JS multer library. :) TODO!
+*/
 final class Router(handler: Handler) extends Routes with LazyLogging:
   @cask.get("/now")
   def now() = Response(Instant.now.toString)
@@ -26,11 +32,11 @@ final class Router(handler: Handler) extends Routes with LazyLogging:
   def saveUser(request: Request) =
     write[UserSaved]( handler.saveUser( read[SaveUser](request.text()) ) )
 
-  @cask.post("/workorder/add")
+  @cask.post("/workorder/add") // only setup for a possible image file
   def addWorkOrder(request: Request) =
     write[WorkOrderAdded]( handler.addWorkOrder( read[AddWorkOrder](request.text()) ) )
 
-  @cask.post("/workorder/save")
+  @cask.post("/workorder/save") // only setup for a possible image file
   def saveWorkOrder(request: Request) =
     write[WorkOrderSaved]( handler.saveWorkOrder( read[SaveWorkOrder](request.text()) ) )
 
