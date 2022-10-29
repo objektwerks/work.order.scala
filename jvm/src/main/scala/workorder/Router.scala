7ce9375227e586e9009410f43a430b88a281d741
objektwerks <objektwerks@runbox.com>
2022-10-29 07:23:45 -0400

    edited router

--- jvm/src/main/scala/workorder/Router.scala
+++ jvm/src/main/scala/workorder/Router.scala
@@ -10,11 +10,10 @@ import upickle.default.{read, write}
 
 import Serializer.given
 
-/* Note: This router can be readily set up as:
-   https://github.com/objektwerks/poolmate.x/blob/main/jvm/src/main/scala/poolmate/Router.scala
-   But this router is a replica of: https://github.com/objektwerks/work.order for various reasons,
-   to include the optional support for images. Image file support is implemented in js/Fetcher. But
-   it is not yet implemented in this router. Scala has no JS multer library. :) TODO!
+/* This router can be set up as: https://github.com/objektwerks/poolmate.x/blob/main/jvm/src/main/scala/poolmate/Router.scala
+   It is a replica of: https://github.com/objektwerks/work.order for various reasons, though, to include the support for images.
+   Image file support is implemented in js/Fetcher. But it is not yet implemented in this router.
+   See: https://com-lihaoyi.github.io/cask/ ( search for: upload )
 */
 final class Router(handler: Handler) extends Routes with LazyLogging:
   @cask.get("/now")
